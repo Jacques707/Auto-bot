370546c14220539902e618214fd7c02c5820fa74
Jacques707 <jacquesiyas@gmail.com>
2024-09-16 18:43:21 +0200

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,66 +1,34 @@
-const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
-async function ai({ message: m, event: e, args: a, usersData: u }) {
-  var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
-`${this.config.name}`
-/*"ai"
-*you can add more prefix here
-*/
-]; 
- if (p.some(b => a[0].toLowerCase().startsWith(b))) {
-try {      
-let prompt = "";
-if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
- const b = await uploadImgbb(e.messageReply.attachments[0].url);
-prompt = a.slice(1).join(" ") + ' ' + b.image.url;
-} else {
- prompt = a.slice(1).join(" ");
-}
- var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
- const r = await require("axios").post(`https://deku-rest-api.gleeze.com/gpt4`, {
-  prompt: prompt,
- apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
-  name: __[0]['tag'],
- id: __[0]['id'],
- });
-var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
- if (r.data.av) {
- if (Array.isArray(r.data.av)) {
- const avs = r.data.av.map(url => getStreamFromURL(url));
- const avss = await Promise.all(avs);
-  m.reply({
- body: _,
- mentions: __,
- attachment: avss
- });
- } else {
- m.reply({
- body: _,
- mentions: __,
-attachment: await getStreamFromURL(r.data.av)
-  });
+const axios = require('axios');
+module.exports.config = {
+  name: 'gpt4o',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['gpt', 'openai'],
+  description: "An AI command powered by GPT-4",
+  usage: "Ai [promot]",
+  credits: 'an',
+  cooldown: 3,
+};
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if (!input) {
+    api.sendMessage(`℘༒𝐂𝐡𝐚𝐭𝐠𝐩𝐭4𝐨 𝐡𝐧𝐫༒℘:\n━━━━━━━━━━━\n\n salut vous êtes sur gpt4o, 𝖯𝗈𝗌𝖾𝗋 𝗆𝗈𝗂 𝗏𝗈𝗍𝗋𝖾 𝗊𝗎𝖾𝗌𝗍𝗂𝗈𝗇.💭`, event.threadID, event.messageID);
+    return;
   }
-  } else {
-m.reply({
-body: _,
-mentions: __
-  });
-  }
+  api.sendMessage(``, event.threadID, event.messageID);
+  try {
+    const {
+      data
+    } = await axios.get(`https://api.kenliejugarap.com/freegpt4o8k/?question=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage('℘༒𝐂𝐡𝐚𝐭𝐠𝐩𝐭4𝐨༒℘:\n━━━━━━━━━━━\n\n' + response + '\n━━━━━━━━━━━\n💨download the AXEL-COPILOT application: https://www.apkfiles.com/apk-615471/axel-copilot-1-0', event.threadID, event.messageID);
   } catch (error) {
- m.reply("Error " + error);
- }
- }
-}
-module.exports = {
-config: {
- name: "ai",
-aliases: [],
-version: 1.6,
-author: "Metoushela walker x June",
-role: 0,
- shortDescription: "An AI that can do various tasks",
- guide: "{pn} <query>",
- category: "AI"
- },
- onStart: function() {},
- onChat: ai
-};￼Enter
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+  }
+};
+￼Enter
